130bcd6aad9f50c8587e4626f409c78c316a391a
André Silva <dev01766@proton.me>
2023-10-08 16:36:43 +0300

    Create 'beans' project.

---
+++ beans/src/test/java/dev/dev01766/beans/BeansApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.dev01766.beans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
